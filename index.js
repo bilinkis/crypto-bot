d836044998b5039a1292a86a6d4bace5fb37fcd0
bilinkis <nico@bilinkis.com>
2016-10-28 17:30:07 +0000

    asd

--- index.js
+++ index.js
@@ -114,26 +114,7 @@ return Httpreq.responseText;
 
     }
 
-function sendSenderAction (recipient, senderAction, cb) {
-    if (!cb) cb = Function.prototype;
 
-    request({
-      method: 'POST',
-      uri: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: "EAAa1VXnZAdXkBALTr0ZAYlopUWZA6u3YWhHpRg4toVpoXQ22lBVxEENqeTIEAsjoRmchI5nIAmScB4UuJ4MGjzQBDsv6SqDyPeAt33J3gHO8hkrFGFxfypS9nJ5RKaKk3ATW2NOZAVOmOhAoBgsZCwNr6SJiZAjrqm0vgUXcLP9AZDZD"},
-      json: {
-        recipient: {
-          id: recipient
-        },
-        sender_action: senderAction
-      }
-    }, (err, res, body) => {
-      if (err) return cb(err)
-      if (body.error) return cb(body.error)
-
-      cb(null, body)
-    })
-  }
 function sendMessage(recipientId, message, sender_action) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
