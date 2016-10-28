97f8e482c80644e2ed940a3763291d0b36b30627
bilinkis <nico@bilinkis.com>
2016-10-28 14:02:18 +0000

    asd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+/*'use strict'
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -62,4 +62,36 @@ function sendMessage(recipientId, message) {
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
-})
+})*/
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+//the imports
+
+app = express()
+app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+app.get('/',function(req,res){
+        if (req.query['hub.verify_token'] === '1234') {
+        res.send(hub.query['hub.challenge'])
+        }
+        res.send('wrong token,error'
+                 )
+        })
+
+
+app.get('/webhook',function(req,res){
+        if (req.query['hub.verify_token'] === '1234') {
+        res.send(hub.query['hub.challenge'])
+        }
+        res.send('wrong token,error'
+                 )
+        })
+
+
+app.listen(app.get('port'), function(req,res) {
+           console.log('server running on port',app.get('port'))
+           })
