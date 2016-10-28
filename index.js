162ece636f395adffa0c7201e985b46f50caf59a
bilinkis <nico@bilinkis.com>
2016-10-28 12:22:57 +0000

    asdasd

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/webhook', function (req, res) {
         wolfram.query(event.message.text, function(err, result) {
             if(err) sendMessage(event.sender.id, {text: err})
             else{
-                console.log(node.get('plaintext').text());
+                console.log(result[1]);
                 sendMessage(event.sender.id, {text: "wait for it"});
                 
                 
