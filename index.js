465c63d374fcf90244fed5c55ce1574b5f3ee844
bilinkis <nico@bilinkis.com>
2016-10-28 13:06:19 +0000

    asdasdasd

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook', function (req, res) {
             if(err) sendMessage(event.sender.id, {text: err})
             else{
                 
-                console.log(result[1].subpods.value.text());
+                console.log(result[0].subpods);
                 
                 sendMessage(event.sender.id, {text: "hola"});
                 
