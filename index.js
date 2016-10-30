48709fa30e7de4bcea67a6560f1e9fa47e68a352
bilinkis <nico@bilinkis.com>
2016-10-30 16:32:10 +0000

    added cryptos

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request')
 const app = express()
 const wolfram = require('wolfram').createClient("6PUVEA-P8K93R4666");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const $ = require('jquery');
 
 app.set('port', (process.env.PORT || 5000))
 
